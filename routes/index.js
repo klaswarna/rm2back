4d3b37ee6031a97cb4e4b6c3a13487f676f24553
klaswarna <klas.warna@gmail.com>
2019-03-14 08:48:29 +0100

    try3

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,10 @@ const bors = require("../models/bors.js");
 const aktier = require("../app.js");
 
 
-// router.get('/', async function(req, res, next) {
-//     var value = await bors.getValue();
-//     const data = {
-//         aktievarde: {
-//             bolag1: value.amount1,
-//             bolag2: value.amount2,
-//             bolag3: value.amount3,
-//             bolag4: value.amount4,
-//             bolag5: value.amount5,
-//         }
-//     };
-//
-//     res.json(data);
-// });
 
 router.get('/', function(req, res, next) {
+    console.log("aktier är: ");
+    console.log(aktier);
     res.json(aktier.aktier);
 });
 
