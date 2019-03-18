4a8a33b14071e75a2f3a513612e272bff76cd583
klaswarna <klas.warna@gmail.com>
2019-03-18 12:30:55 +0100

    changed test-database

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 
 module.exports = (function () {
     if (process.env.NODE_ENV === 'test') {
-        return new sqlite3.Database('./db/rm2db-test.sqlite');
+        return new sqlite3.Database('./db/rm2db-test2.sqlite');
     }
 
     return new sqlite3.Database('./db/rm2db.sqlite');
